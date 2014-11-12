dbb32e60dc67eb8b0398f47212453cb10b3c914e
Oscar Stanley <40785@longroad.ac.uk>
2014-11-12 15:16:59 +0000

    DONE

---
+++ amout in word.py
@@ -0,0 +1,4 @@
+# amout of letters in word
+msg = input("please enter a message that you want to be counted: ")
+number =len(msg)
+print("The amount of numbers in the message {0} is {1}".format(msg,number))
