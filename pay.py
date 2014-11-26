df9a95f949dc4004b38724b16556854032ac083c
40785 <40785@PC004144.coll.lr.local>
2014-11-26 15:14:51 +0000

    done

---
+++ pay.py
@@ -0,0 +1,43 @@
+# Get the data from the user 
+def get_data():
+    hours = int(input("Please enter the numbers of hours you do: "))
+    rate = int(input("Please enter the rate of pay you have: "))
+    return hours,rate
+
+
+#calculate basic pay
+def calculate_basic_pay(hours,rate):
+    total = hours * rate
+    return total
+
+#calculate the over time pay
+
+def calculate_overtime(hours,rate):
+    over_time = hours - 40
+    basic_pay = 40 * rate
+    overtime_pay = over_time * rate * 1.5
+    total = overtime_pay + basic_pay
+    return total
+# the statment 
+def calculate_total_pay(hours,rate):
+    if hours <= 40:
+        total = calculate_basic_pay(hours,rate)
+    else:
+        total = calculate_total_pay(hours,rate)
+    return total
+
+# Get the data from the user
+def get_data():
+    hours = int(input("Please enter the numbers of hours you do: "))
+    rate = int(input("Please enter the rate of pay you have: "))
+    return hours , rate 
+
+#main program
+total_pay = calculate_basic_pay(hours,rate)
+print("your basic pay with out over time is {0}".format(total_pay))
+
+total_pay = calculate_overtime(hours,rate)
+print("your over time pay with over time is {0}".format(total_pay))
+
+
+
