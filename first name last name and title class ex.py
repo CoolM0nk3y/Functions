473ebe2df4db8f8f7c28fc97766cafaf0911ab78
40785 <40785@PC004144.coll.lr.local>
2014-12-08 10:30:28 +0000

    done

--- first name last name and title class ex.py
+++ first name last name and title class ex.py
@@ -1,30 +1,26 @@
 def name():
-    input("Please enter your first name: ")
+    fname = input("Please enter your first name: ")
+    return fname
 
 
 def last_name():
-            input("Please enter your last name: ")
+    lname = input("Please enter your last name: ")
+    return lname
         
 def title_name ():
-    input ("Please enter your title e.g Mr Miss Mrs: ")
-     
-def get_data ():
-    fname = name()
-    lname = last_name()
-    title = title_name()
-    return title , fname , lname
-
-def message (title , fname , lname):
-    message = print("Welcome {0} {1} {2}".format(title , fname , lname))
-    return message
-
-def display(get_data , message):
-    get_data()
-    print (message)
-
-
-display (get_data , message)
-
-
- 
+    title = input ("Please enter your title e.g Mr Miss Mrs: ")
+    return title
     
+     
+def message (title , fname , lname):
+    newmessage ="Welcome {0} {1} {2}".format(title , fname , lname)
+    return newmessage
+
+    
+
+fname = name()
+lname = last_name()
+title = title_name()
+newmessage = message (title , fname , lname)
+newmessage = newmessage.title()
+print(newmessage)
---
+++ perint mesage.py
@@ -0,0 +1,10 @@
+def data():
+    value = input("please enter a message: ")
+    return value
+
+def message(value):
+    print(value)
+    
+value = data()
+message(value)
+
